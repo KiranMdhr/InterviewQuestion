30cb28c2f7d2eccc2545cee94f120b3a2a53b94c
Kiran Mdhr <kiranmdhr9@gmail.com>
2025-03-21 15:50:18 +0545

    first commit

---
+++ src/Qno1.java
@@ -0,0 +1,60 @@
+public class Qno1 {
+
+
+    public static int mergeAndCount(int[] arr, int[] temp, int left, int mid, int right) {
+        int i = left;
+        int j = mid + 1;
+        int k = left;
+        int count = 0;
+
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+                count += (mid - i + 1); // Count inversions
+            }
+        }
+
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+
+        while (j <= right) {
+            temp[k++] = arr[j++];
+        }
+
+
+        for (i = left; i <= right; i++) {
+            arr[i] = temp[i];
+        }
+
+        return count;
+    }
+
+    public static int mergeSortAndCount(int[] arr, int[] temp, int left, int right) {
+        int invCount = 0;
+        if (left < right) {
+            int mid = (left + right) / 2;
+
+            invCount += mergeSortAndCount(arr, temp, left, mid);
+            invCount += mergeSortAndCount(arr, temp, mid + 1, right);
+            invCount += mergeAndCount(arr, temp, left, mid, right);
+        }
+        return invCount;
+    }
+
+    public static int countInversions(int[] arr) {
+        int n = arr.length;
+        int[] temp = new int[n];
+        return mergeSortAndCount(arr, temp, 0, n - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {8, 4, 2, 1,5,7};
+        int totalInversions = countInversions(arr);
+        System.out.println("Total Inversions: " + totalInversions);
+    }
+
+
+}
